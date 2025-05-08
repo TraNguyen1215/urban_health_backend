e13ff2545d42154e1d4c3b80c919f5311271d04e
TraNguyen1215 <tratthunguyen1215@gmail.com>
2025-05-09 04:23:49 +0700

    lấy csyt theo tên

--- src/api/data/routes.py
+++ src/api/data/routes.py
@@ -104,3 +104,47 @@ def get_facility_by_type(facility_type):
     finally:
         if conn:
             conn.close()
+
+# tìm cơ sở y tế theo tên
+@data_bp.route('/facilities/search', methods=['GET'])
+def search_facility_by_name():
+    name = request.args.get('name')
+    if not name:
+        return jsonify({"error": "Invalid or missing 'name' parameter"}), 400
+
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        
+        sql = """SELECT * FROM access_health WHERE name ILIKE %s"""
+        
+        cur.execute(sql, ('%' + name + '%',))
+        result = cur.fetchall()
+        
+        if not result:
+            return jsonify({"message": "No facilities found"}), 404
+        
+        return jsonify({
+            "data": [
+                {
+                    "osm_id": row[0],
+                    "type": row[1],
+                    "name": row[3],
+                    "healthcare_speciality": row[2] if row[2] else 'chung',
+                    "address": row[11],
+                    "opening_hours": row[4],
+                    "operator": row[5],
+                    "operator_type": row[6],
+                    "phone": row[7],
+                    "website": row[8],
+                    "wheelchair": row[9],
+                } for row in result
+            ]
+        })
+
+    except Exception as e:
+        return jsonify({"error": "Failed to fetch or process facility data", "details": str(e)}), 500
+    
+    finally:
+        if conn:
+            conn.close()
